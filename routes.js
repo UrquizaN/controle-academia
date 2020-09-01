dd9b7e6f2721fc85e081752e4e20111a0f90e22f
Urquiza <urquiza-nobrega@outlook.com>
2020-08-31 21:37:54 -0300

    structured project

--- routes.js
+++ routes.js
@@ -2,7 +2,15 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
-    return res.send('Ok')
+    return res.redirect('/instructors')
+})
+
+routes.get('/instructors', (req, res) => {
+    return res.render('instructors/index')
+})
+
+routes.get('/members', (req, res) => {
+    return res.send('members')
 })
 
 module.exports = routes

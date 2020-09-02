e2255b42ae6528e84a0db9aa530258f44eb70652
Urquiza <urquiza-nobrega@outlook.com>
2020-09-02 13:31:52 -0300

    feat: routes and instructor form

--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const routes = require('./routes')
 
 const server = express()
 
+server.use(express.urlencoded({ extended: true }))
 server.use(express.static('public'))
-server.set("view engine", "njk")
 server.use(routes)
+
+server.set("view engine", "njk")
 
 nunjucks.configure("views", {
     express: server,
---
+++ utils.js
@@ -0,0 +1,14 @@
+module.exports = {
+    age: function(timestamp) {
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+        let age = today.getFullYear() - birthDate.getFullYear(timestamp)
+        const month = today.getMonth() - birthDate.getMonth(timestamp)
+
+        if(month < 0 || month == 0 && today.getDate() < birthDate.getDate())
+            age -= 1
+        
+        return age
+    }
+}

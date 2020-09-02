4cc2bff377bac104e6bd2aa22fabe3682ddc5c36
Urquiza <urquiza-nobrega@outlook.com>
2020-09-02 16:33:30 -0300

    feat: edit instructor page

--- instructors.js
+++ instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
-const { age } = require('./utils')
+const { age, date } = require('./utils')
 
 exports.show = function(req, res) {
     const id = req.params.id
@@ -48,8 +48,27 @@ exports.post = function(req, res) {
     fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
         if (err) return res.send('Write file error')
        
-        return res.redirect("/instructors")
+        return res.redirect(`instructors/${id}`)
     })
 
+
+
     // return res.send(req.body)
+}
+
+exports.edit = function(req, res) {
+    const id = req.params.id
+
+    const foundInstructor = data.instructors.find((instructor) => {
+        return instructor.id == id
+    })
+
+    if(!foundInstructor) return res.send('Instructor not found!')
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', { instructor })
 }

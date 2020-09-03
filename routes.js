8dd8003eec56bcce91403065aff33e34c7e67fbb
Urquiza Nobrega F. Brito da Silva <urquizasilva@bahiana.intranet>
2020-09-03 14:30:11 -0300

    feat: refactoring and added edit instructor page

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const instructors = require('./instructors')
 
 routes.get('/', (req, res) => {
     return res.redirect('/instructors')
@@ -9,13 +10,17 @@ routes.get('/instructors', (req, res) => {
     return res.render('instructors/index')
 })
 
-routes.get('/instructors/create', (req, res) => {
+routes.get('/instructors/create', function(req, res) {
     return res.render('instructors/create')
 })
 
-routes.post('/instructors', (req, res) => {
-    return res.send('Recebido')
-})
+routes.get('/instructors/:id', instructors.show)
+
+routes.get('/instructors/:id/edit', instructors.edit)
+
+routes.post('/instructors',instructors.post)
+
+routes.put('/instructors',instructors.put)
 
 routes.get('/members', (req, res) => {
     return res.send('members')

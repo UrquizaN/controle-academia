1d2df0bdffb9d2c8a84f68ed586bc570a063f963
Urquiza <urquiza-nobrega@outlook.com>
2020-09-05 12:16:18 -0300

    feat: delete confirmation

--- public/scripts.js
+++ public/scripts.js
@@ -5,3 +5,11 @@ for(item of menuItems){
     if(currentPage.includes(item.getAttribute('href')))
     item.classList.add('active')
 }
+
+const formDelete = document.querySelector('#form-delete')
+
+formDelete.addEventListener("submit", function(envent){
+    const confirmation = confirm("Deseja realmente deletar?")
+
+    if(!confirmation) event.preventDefault()
+})

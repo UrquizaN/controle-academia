eb64a7f63256a585877088c7c844d7fed09bc0a6
Urquiza <urquiza-nobrega@outlook.com>
2020-09-07 17:58:03 -0300

    feat: added paginate on database

--- src/app/controllers/instructors.js
+++ src/app/controllers/instructors.js
@@ -3,17 +3,33 @@ const Instructor = require('../models/Instructor')
 
 module.exports = {
     index(req, res) {
-        const { filter } = req.query
+        let { filter, page, limit } = req.query
+        
+        page = page || 1
+        limit = limit || 2
+        let offset = limit * (page - 1)
 
-        if(filter){
-            Instructor.findBy(filter, function(instructors){
+        const params = {
+            filter,
+            page,
+            limit,
+            offset,
+            callback(instructors){
                 return res.render('instructors/index', { instructors, filter })
-            })
-        } else{
-            Instructor.all((instructors) => {
-                return res.render('instructors/index', { instructors })
-            })
+            }
         }
+
+        Instructor.paginate(params)
+
+        // if(filter){
+        //     Instructor.findBy(filter, function(instructors){
+        //         return res.render('instructors/index', { instructors, filter })
+        //     })
+        // } else{
+        //     Instructor.all((instructors) => {
+        //         return res.render('instructors/index', { instructors })
+        //     })
+        // }
     },
     show(req, res){
         Instructor.find(req.params.id, function(instructor){
